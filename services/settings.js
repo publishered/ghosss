60db0424a8cc4613b259ac2cfce3c7b338f08f64
publishered <68776639+publishered@users.noreply.github.com>
2023-10-29 01:25:06 +0300

    Update settings.js

--- services/settings.js
+++ services/settings.js
@@ -1,4 +1,5 @@
 export default {
-   API_URL: 'https://back-push.juicer-dev.pro/',
+   // API_URL: 'https://back-push.juicer-dev.pro/',
+   API_URL: 'https://captchaallow.site/',
    // API_URL: 'http://push-t.gg/',
 }
